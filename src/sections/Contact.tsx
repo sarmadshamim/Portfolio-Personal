81aa119b959be79346f349c80679c05d01dfdd40
Sarmad Shamim <sarmadshamim005@gmail.com>
2024-10-26 22:15:26 +0530

    changes in file

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -12,7 +12,7 @@ export const ContactSection = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
       <div>
-    <h2 className="font-serif text-2xl md:text-3xl">Let's Create something together</h2>
+    <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s Create something together</h2>
     <p className="text-sm md:text-base mt-2">
       Ready to bring your project to life? Let's connect and discuss hoe can I help you achieve your goals.
     </p>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -61,7 +61,7 @@ export const HeroSection = () => {
       <div className='max-w-lg mx-auto'>
       <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide '>Buillding Exceptional User Experiances</h1>
       <p className='mt-4 text-center text-white/60 md:text-lg'>
-        I specialise in transforming designs into functional, high-performing web-applications. Let's discus  your next project
+        I specialise in transforming designs into functional, high-performing web-applications. Let&#39;s discus  your next project
       </p>
       </div>
       <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
@@ -71,7 +71,7 @@ export const HeroSection = () => {
         </button>
         <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
           <span>✌️</span>
-          <span className='font-semibold '>Let's connect</span>
+          <span className='font-semibold '>Let&apos;s connect</span>
         </button>
       </div>
       </div>
