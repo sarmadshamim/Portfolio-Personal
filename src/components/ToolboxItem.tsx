45e7e1f22a38e071463c24ea0f382d4070822f79
Sarmad Shamim <sarmadshamim005@gmail.com>
2024-10-26 18:10:26 +0530

    create toolbox component

--- src/components/ToolboxItem.tsx
+++ src/components/ToolboxItem.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { TechIcon } from "./TechIcon";
 import { twMerge } from "tailwind-merge";
 export const ToolboxItem = ({toolboxItem,className,itemsWrapperClassName}:{
@@ -12,13 +13,16 @@ export const ToolboxItem = ({toolboxItem,className,itemsWrapperClassName}:{
     return(
         <div className={twMerge("flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]",className)}>
             <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6",itemsWrapperClassName)}>
-        {toolboxItem.map((item) => (
-          <div key={item.title} className="inline-flex items-center gap-4 py-2 px-3 outline-2 outline-white/10 rounded-lg">
-            <TechIcon component ={item.iconType} />
-            <span className="font-semibold">{item.title}</span>  
-          </div>
-
-        ))}
+              {...new Array(2).fill(0).map((_,idx) => (
+                <Fragment key={idx}>
+                {toolboxItem.map((item) => (
+                 <div key={item.title} className="inline-flex items-center gap-4 py-2 px-3 border border-emerald-400/20 outline-2 outline-white/10 rounded-lg">
+                 <TechIcon component ={item.iconType} />
+                  <span className="font-semibold">{item.title}</span>  
+                 </div>
+                 ))}
+              </Fragment>
+            ))}
       </div>
       </div>
     )
