3dfc0abde05cfa14d7378a8796abe3d5cbd5cc26
Sarmad Shamim <sarmadshamim005@gmail.com>
2024-10-26 18:08:23 +0530

    create card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
 export const Card = ({className, children,...other}: ComponentPropsWithoutRef<"div">) => {
     return (
-        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20  after:pointer-events-none",className)}{...other}>
+        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20 border border-white/20 after:pointer-events-none",className)}{...other}>
         <div className="absolute inset-0 -z-10 opacity-5" style={{
         backgroundImage: `url(${grainImage})`
         }}></div>
